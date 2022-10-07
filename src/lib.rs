ffae401a97e95ef8bd556e0bec24ca53884c9826
jmartin <andriantsiferanamartin.ext@orange.com>
2022-10-07 11:30:21 +0300

    Vector Math operator

--- src/lib.rs
+++ src/lib.rs
@@ -61,3 +61,49 @@ mod object_op {
         }
     }
 }
+
+mod vecteur {
+
+    use std::ops::Add;
+
+    #[allow(dead_code)]
+    fn round(decimal: f32, precision: u8) -> f32 {
+        let p = 10u8.pow(precision as u32) as f32;
+        (decimal * p).round() / p
+    }
+
+    #[derive(Debug, PartialEq)]
+    struct Vecteur<X, Y> {
+        x: X,
+        y: Y,
+    }
+
+    impl<X, Y> Add for Vecteur<X, Y>
+    where
+        X: Add<Output = X>,
+        Y: Add<Output = Y>,
+    {
+        type Output = Self;
+
+        fn add(self, rhs: Self) -> Self {
+            return Self {
+                x: self.x + rhs.x,
+                y: self.y + rhs.y,
+            };
+        }
+    }
+
+    #[test]
+    fn test_add_vecteur() {
+        fn round2(decimal: f32) -> f32 {
+            round(decimal, 2)
+        }
+
+        let ab = Vecteur { x: 1.5, y: 5 };
+        let bc = Vecteur { x: -2.2, y: 3 };
+        let ac = ab + bc;
+        let expected = Vecteur { x: -0.7, y: 8 };
+        assert_eq!(round2(ac.x as f32), round2(expected.x as f32));
+        assert_eq!(round2(ac.y as f32), round2(expected.y as f32));
+    }
+}
